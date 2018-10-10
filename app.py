6c1d1a84cc605ed7bbd16ace9bfd688f5c13e9a0
Simon Kolotov <simonkolotov@gmail.com>
2018-10-09 23:22:37 -0700

    Added command-line argument parsing, creation of directory for audiobook, overwrite on flag on prompt and deletion on error on flag or prompt

--- app.py
+++ app.py
@@ -6,18 +6,16 @@ import json
 import re
 import sys
 
+import os
+import shutil
+import argparse
+
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-USAGE_HELP = '''
-    Audioknigi.club book downloader.
-    Usage: {0} <url>
-        url: a URL of a multi-part audio book.
-'''
 
 AJAX_ON_SUCCESS = '''
     $(document).ajaxSuccess(function(event, xhr, opt) {
@@ -64,23 +62,77 @@ def download_chapter(url):
     """Download a chapter."""
     return requests.get(url).content
 
+def yes_or_no(question):
+    while "the answer is invalid":
+        reply = str(input(question+' (y/n): ')).lower().strip()
+        if reply[:1] == 'y':
+            return True
+        if reply[:1] == 'n':
+            return False
 
 # start the app
 if __name__ == '__main__':
 
-    if len(sys.argv) != 2:
-        print(USAGE_HELP.format(*sys.argv))
-        sys.exit(1)
+    #set up command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--outputDir',
+                        help='Directory the AudioBook will be dowloaded to. Default: <AudioBookName>',
+                        metavar='OutputDirectory',
+                        type=str)
+    parser.add_argument('audioBookURL',
+                        help='a URL of a multi-part audio book',
+                        metavar='URL')
+    parser.add_argument('-w', '--overwrite',
+                        help='overwrite existing directory without asking',
+                        action='store_true',
+                        default=False)
+    parser.add_argument('-c', '--cleanup',
+                        help='delete outputDir in case of errors',
+                        action='store_true',
+                        default=False)
 
-    book_url = sys.argv[1]
+    #parse arguments
+    args = parser.parse_args()
 
-    with open_browser(book_url) as browser:
-        book_id = get_book_id(browser.page_source)
-        playlist = get_playist(browser, book_id)
+    #default value for output dir is the book name
+    if args.outputDir is None:
+        args.outputDir = args.audioBookURL.split('/')[-1]
 
-    for url, fname in playlist:
-        print('Downloading chapter "{}"'.format(fname))
-        with open('{}.mp3'.format(fname), 'wb') as outfile:
-            outfile.write(download_chapter(url))
+    #create directory if required
+    if not os.path.exists(os.path.abspath(args.outputDir)):
+      print ("Creating directory <{}>".format(os.path.abspath(args.outputDir)))
+      os.makedirs(os.path.abspath(args.outputDir))
 
-    print('All done.')
+    if (os.listdir(os.path.abspath(args.outputDir)) and not args.overwrite):
+        if not yes_or_no('Directory <{}> exists. Overwrite?'.format(os.path.abspath(args.outputDir))):
+          sys.exit(1)
+    else:
+        print("overwriting files in <{}>\n".format(os.path.abspath(args.outputDir)))
+
+    print("\nDownloading audiobook from {} to <{}>\n".format(args.audioBookURL,
+                                                           os.path.abspath(args.outputDir)))
+
+    try:
+        with open_browser(args.audioBookURL) as browser:
+              book_id = get_book_id(browser.page_source)
+              playlist = get_playist(browser, book_id)
+
+        for url, fname in playlist:
+            print('Downloading chapter "{}"'.format(fname))
+            with open('{}.mp3'.format(os.path.join(os.path.abspath(args.outputDir),fname)), 'wb') as outfile:
+                outfile.write(download_chapter(url))
+
+        print('All done\n')
+
+    except Exception as e:
+      #remove previously created directory
+      if (args.cleanup or
+          (os.listdir(os.path.abspath(args.outputDir)) and
+           yes_or_no('Error encountered. Remove Audiobook directory?')
+          )
+         ):
+          print("Deleting directory <{}>\n".format(os.path.abspath(args.outputDir)))
+          shutil.rmtree(os.path.abspath(args.outputDir))
+
+      #raise the same exception to print the error
+      raise e
