09eed2fbc063d820e09b5d1699b386fc64887936
Danil Kister <danil.kister@embl.de>
2025-02-02 21:56:34 +0100

    Performance improvement: download segments concurrently.

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 """
-    Download complete audio books from audioknigi.ru
+Download complete audio books from audioknigi.ru
 """
+
 from contextlib import contextmanager, suppress
-from functools import partial
-from itertools import count
-from pathlib import Path
+from dataclasses import dataclass
+from functools import cache, cached_property
+from pathlib import Path, PurePosixPath
 import os
 import sys
+from tempfile import TemporaryDirectory
 from urllib.parse import urlparse
 
-from typing import Callable, Iterable, Mapping
+from typing import Callable, Mapping
 
 from Crypto.Cipher import AES
 import click
@@ -19,9 +21,59 @@ import requests
 from pathvalidate import sanitize_filename  # type:ignore
 from selenium.webdriver.firefox.options import Options
 from selenium import webdriver  # type:ignore
-from tenacity import (Retrying, RetryError, retry, retry_if_exception_type,
-                      stop_after_attempt, wait_fixed)
-import tqdm
+from streamable import Stream, star
+from tenacity import (
+    Retrying,
+    RetryError,
+    retry,
+    retry_if_exception_type,
+    stop_after_attempt,
+    wait_fixed,
+)
+
+
+@dataclass
+class Chapter:
+    """
+    A downladable, decrypted book chapter.
+    """
+
+    segment: m3u8.Segment
+    key: bytes
+
+    @property
+    def name(self) -> str:
+        """
+        Return the chapter's file name.
+        """
+        path = urlparse(self.segment.absolute_uri).path
+        return PurePosixPath(path).name
+
+    @cached_property
+    def decrypt(self) -> Callable[[bytes], bytes]:
+        """
+        Return an AES decryptor function for the chapter.
+        """
+        iv = bytes.fromhex(self.segment.key.iv.lstrip('0x'))
+        return AES.new(self.key, AES.MODE_CBC, IV=iv).decrypt
+
+    @retry(
+        stop=stop_after_attempt(3),
+        wait=wait_fixed(3),
+        retry=retry_if_exception_type(requests.exceptions.ConnectionError),
+        after=lambda s: click.echo(
+            f'Waiting for server response [{ s.attempt_number }]'
+        ),
+    )
+    def download(self, prefix: str) -> Path:
+        """
+        Download the chapter, decrypt and save to the specified path.
+        """
+        path = Path(prefix) / self.name
+        with open(path, mode='wb') as file:
+            for chunk in requests.get(self.segment.absolute_uri, stream=True, timeout=10):
+                file.write(self.decrypt(chunk))
+        return path
 
 
 def get_book_title(url: str) -> str:
@@ -43,23 +95,6 @@ def get_m3u8_url(browser) -> str:
     raise FileNotFoundError
 
 
-def get_signed_playlist_url(url: str) -> str:
-    """
-    Open book page in the browser, get the signed playlist URL.
-    """
-    retry_opt: Mapping = {
-        'stop': stop_after_attempt(20),
-        'wait': wait_fixed(5),
-        'retry': retry_if_exception_type(FileNotFoundError),
-    }
-    with open_browser(url) as browser:
-        for retry_no, attempt in enumerate(Retrying(**retry_opt), start=1):
-            with attempt, suppress(RetryError):
-                return get_m3u8_url(browser)
-            click.echo(f'Waiting for playlist [{ retry_no }]')
-    raise click.Abort('Failed to fetch playlist.')
-
-
 @contextmanager
 def open_browser(url):
     """
@@ -78,6 +113,23 @@ def open_browser(url):
         browser.close()
 
 
+def get_signed_playlist_url(url: str) -> str:
+    """
+    Open book page in the browser, get the signed playlist URL.
+    """
+    retry_opt: Mapping = {
+        'stop': stop_after_attempt(20),
+        'wait': wait_fixed(5),
+        'retry': retry_if_exception_type(FileNotFoundError),
+    }
+    with open_browser(url) as browser:
+        for retry_no, attempt in enumerate(Retrying(**retry_opt), start=1):
+            with attempt, suppress(RetryError):
+                return get_m3u8_url(browser)
+            click.echo(f'Waiting for playlist [{ retry_no }]')
+    raise click.Abort('Failed to fetch playlist.')
+
+
 def get_or_create_output_dir(dirname: str, book_title: str) -> Path:
     """
     Create or reuse output directory in a fail-safe manner.
@@ -92,59 +144,31 @@ def get_or_create_output_dir(dirname: str, book_title: str) -> Path:
     return path
 
 
-def file_factory(prefix: Path, title: str, merge: bool) -> Iterable[Callable]:
+@cache
+def get_key(segment: m3u8.Segment) -> bytes:
     """
-    A factory producing file openers for the audio segments.
-    Always append to one single file if segment merging is requested.
+    Download decryption key for the segment.
     """
-    mode = 'ab' if merge else 'wb'
-    for index in count(start=1):
-        stem = f'{title}' if merge else f'{title}-{index:03}'
-        path = (prefix / stem).with_suffix('.ts')
-        yield partial(open, path, mode)
-
-
-@retry(stop=stop_after_attempt(3), wait=wait_fixed(3),
-       retry=retry_if_exception_type(requests.exceptions.ConnectionError),
-       after=lambda s: click.echo(f'Waiting for server response [{ s.attempt_number }]'))
-def http_get(url: str, **kwargs) -> requests.Response:
-    """
-    Make an HTTP get request, return response.
-    Retry on connection failures.
-    """
-    return requests.get(url, timeout=10, **kwargs)
-
-
-def get_key(url: str) -> bytes:
-    """
-    Download decryption key.
-    """
-    resp = requests.get(url, timeout=10)
+    resp = requests.get(segment.key.absolute_uri, timeout=10)
     if resp.status_code != 200:
         raise click.Abort('Could not fetch decryption key.')
     return resp.content
 
 
-def make_cipher_for_segment(segment):
-    """
-    Initialize an AES decryptor.
-    """
-    key = get_key(segment.key.absolute_uri)
-    iv = bytes.fromhex(segment.key.iv.lstrip('0x'))
-    return AES.new(key, AES.MODE_CBC, IV=iv)
-
-
-def convert_to_mp3(stream_path: Path) -> None:
+def convert_to_mp3(stream_path: str) -> None:
     """
     Attempt to convert .ts file to .mp3 with `ffmpeg`.
     """
-    mp3_path = str(stream_path.with_suffix('.mp3'))
+    ts_path = Path(stream_path)
+    mp3_path = ts_path.with_suffix('.mp3')
     try:
-        ffmpeg.input(stream_path).output(mp3_path).run()
+        ffmpeg.input(str(ts_path)).output(str(mp3_path)).run()
     except FileNotFoundError:
-        click.echo("Warning: `ffmpeg` is not installed, won't convert to MP3.")
+        click.secho(
+            "Warning: `ffmpeg` is not installed, won't convert to MP3.", fg='magenta'
+        )
     else:
-        stream_path.unlink()
+        ts_path.unlink()
 
 
 def confirm_overwrite(overwrite: bool, path: Path) -> None:
@@ -159,35 +183,56 @@ def confirm_overwrite(overwrite: bool, path: Path) -> None:
         raise click.Abort('Terminated.')
 
 
-@click.command()
+@click.command(context_settings={'show_default': True})
 @click.argument('audio_book_url')
 @click.option(
-    '-o', '--output-dir', 'output_dir', default=None,
-    help=('Download directory. Default: <audio-book-title>')
+    '-o',
+    '--output-dir',
+    'output_dir',
+    default=None,
+    help='Path to download directory',
 )
 @click.option(
-    '-y', '--yes', 'force_overwrite', is_flag=True,
-    help='Overwrite existing files without a prompt.'
+    '-y',
+    '--yes',
+    'force_overwrite',
+    is_flag=True,
+    help='Overwrite existing files without a prompt',
 )
-def cli(audio_book_url, output_dir, force_overwrite):
+@click.option('-c', '--concurrency', type=int, default=8, help='Download segments concurrently')
+def cli(audio_book_url: str, output_dir: str, force_overwrite: bool, concurrency: int):
     """
     Download the complete book.
     """
     book_title = get_book_title(audio_book_url)
     path = get_or_create_output_dir(output_dir, book_title)
     confirm_overwrite(force_overwrite, path)
-    click.echo('Fetching book metadata, please stand by...')
+    click.secho('Fetching book metadata, please stand by...', fg='green')
     playlist_url = get_signed_playlist_url(audio_book_url)
     segments = m3u8.load(playlist_url).segments
-    stream_path = (path / book_title).with_suffix('.ts')
-    with open(stream_path, mode='wb') as file:
-        bar_format = 'Downloading segment {n}/{total} [{elapsed}]'
-        for segment in tqdm.tqdm(segments, bar_format=bar_format):
-            cipher = make_cipher_for_segment(segment)
-            for chunk in http_get(segment.absolute_uri, stream=True):
-                file.write(cipher.decrypt(chunk))
-    convert_to_mp3(stream_path)
-    click.echo(f'Finished, check the { path } directory.\n')
+    with (
+        TemporaryDirectory() as tmp_path,
+        open((path / book_title).with_suffix('.ts'), mode='wb') as merged,
+    ):
+        keys = (
+            Stream(segments)
+            .map(get_key, concurrency=concurrency)
+            .observe('keys')
+        )
+        segments = (
+            Stream(zip(segments, keys))
+            .map(star(lambda seg, key: Chapter(segment=seg, key=key)))
+            .map(lambda c: c.download(prefix=tmp_path), concurrency=concurrency)
+            .observe('segments')
+            .map(lambda file: file.read_bytes())
+            .foreach(merged.write)
+            .observe('files')
+        )
+        click.secho('Downloading segments...', fg='yellow')
+        segments.count()
+    convert_to_mp3(merged.name)
+    click.secho(f'Finished, check the { path } directory.\n', fg='green')
+
 
 if __name__ == '__main__':
     # pylint:disable=no-value-for-parameter
